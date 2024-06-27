4a296e41634425386bc09aaa5b748fa4c7953d04
Emre Gürkan <emregr99@gmail.com>
2024-06-27 17:28:02 +0300

    Added geometry collection and Blueprint Native Events

--- Source/Slash/Public/Items/Weapons/Weapon.h
+++ Source/Slash/Public/Items/Weapons/Weapon.h
@@ -27,6 +27,9 @@ protected:
 
 	UFUNCTION()
 	void OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void CreateFields(const FVector& FieldLocation);
 public:
 	void Equip(USceneComponent* InParent, FName InSocketName);
 	void AttachMeshToSocket(USceneComponent* InParent, FName InSocketName);
