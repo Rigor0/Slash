586de3638b9970320f1e6c03a298890158c74eaf
Emre Gürkan <emregr99@gmail.com>
2024-07-07 00:01:20 +0300

    BreakableActor modification

--- Source/Slash/Private/Breakable/BreakableActor.cpp
+++ Source/Slash/Private/Breakable/BreakableActor.cpp
@@ -3,6 +3,8 @@
 
 #include "Breakable/BreakableActor.h"
 #include "GeometryCollection/GeometryCollectionComponent.h"
+#include "Items/Treasure.h"
+#include "Components/CapsuleComponent.h"
 
 ABreakableActor::ABreakableActor()
 {
@@ -10,11 +12,14 @@ ABreakableActor::ABreakableActor()
 
 	GeometryCollection = CreateDefaultSubobject<UGeometryCollectionComponent>(TEXT("GeometryCollection"));
 	SetRootComponent(GeometryCollection);
-
 	GeometryCollection->SetGenerateOverlapEvents(true);
-	GeometryCollection->ObjectType = EObjectStateTypeEnum::Chaos_Object_Sleeping;
-
 	GeometryCollection->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+	GeometryCollection->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	Capsule->SetupAttachment(GetRootComponent());
+	Capsule->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	Capsule->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Block);
 }
 
 void ABreakableActor::BeginPlay()
@@ -31,6 +36,12 @@ void ABreakableActor::Tick(float DeltaTime)
 
 void ABreakableActor::GetHit_Implementation(const FVector& ImpactPoint)
 {
-
+	UWorld* World = GetWorld();
+	if (World && TreasureClass)
+	{
+		FVector Location = GetActorLocation();
+		Location.Z += 75.f;
+		World->SpawnActor<ATreasure>(TreasureClass, Location, GetActorRotation());
+	}
 }
 
--- Source/Slash/Public/Breakable/BreakableActor.h
+++ Source/Slash/Public/Breakable/BreakableActor.h
@@ -23,7 +23,14 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
-private:	
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	UGeometryCollectionComponent* GeometryCollection;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class UCapsuleComponent* Capsule; 
+
+private:	
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class ATreasure> TreasureClass;
 };
